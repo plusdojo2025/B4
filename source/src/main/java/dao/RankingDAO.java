4aef765ba959c16467d9ff5efdc8ec70249484db
plusdojo2025-kato-toya <kato-toya-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-24 11:30:05 +0900

    DAO

--- source/src/main/java/dao/RankingDAO.java
+++ source/src/main/java/dao/RankingDAO.java
@@ -21,7 +21,7 @@ public class RankingDAO{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/B4?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/b4?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
 			
